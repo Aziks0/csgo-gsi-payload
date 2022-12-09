c48daab405d3e7033d9f0bb07f253e8074669ea2
Benjamin DIDIER <contact@aziks.aleeas.com>
2022-12-09 18:59:34 +0100

    feat: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! CSGO Game State Integration documentation coming partially from
+//! [u/Bkid](reddit_doc) — __[Archive](reddit_doc_archive)__
+//!
+//! [reddit_doc]: https://www.reddit.com/r/GlobalOffensive/comments/cjhcpy/game_state_integration_a_very_large_and_indepth/
+//! [reddit_doc_archive]: https://web.archive.org/web/20220906050651/https://www.reddit.com/r/GlobalOffensive/comments/cjhcpy/game_state_integration_a_very_large_and_indepth/
+
