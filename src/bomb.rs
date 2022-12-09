1283eba5c3e588dc19d632da59f91f34269e73dd
Benjamin DIDIER <contact@aziks.aleeas.com>
2022-12-09 23:08:17 +0100

    fix: use float instead of string where possible

--- src/bomb.rs
+++ src/bomb.rs
@@ -1,31 +1,39 @@
 use serde::Deserialize;
+use serde_with::{serde_as, DisplayFromStr};
 
 use super::custom::coordinates::Coordinates;
 
+#[serde_as]
 #[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "lowercase")]
 #[serde(tag = "state")]
 pub enum Bomb {
     Carried {
         position: Coordinates,
-        player: String, // a SteamID64
+        /// a SteamID64
+        player: String,
     },
     Dropped {
         position: Coordinates,
     },
     Planting {
         position: Coordinates,
-        player: String,    // a SteamID64
-        countdown: String, // FIXME: float doesn't work
+        /// a SteamID64
+        player: String,
+        #[serde_as(as = "DisplayFromStr")]
+        countdown: f64,
     },
     Planted {
         position: Coordinates,
-        countdown: String, // FIXME: float doesn't work
+        #[serde_as(as = "DisplayFromStr")]
+        countdown: f64,
     },
     Defusing {
         position: Coordinates,
-        player: String,    // a SteamID64
-        countdown: String, // FIXME: float doesn't work
+        /// a SteamID64
+        player: String,
+        #[serde_as(as = "DisplayFromStr")]
+        countdown: f64,
     },
     Defused {
         position: Coordinates,
--- src/phase_countdowns.rs
+++ src/phase_countdowns.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use serde_with::{serde_as, DisplayFromStr};
 
 #[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "lowercase")]
@@ -15,8 +16,10 @@ pub enum Phase {
     TimeoutCT,
 }
 
+#[serde_as]
 #[derive(Deserialize, Debug, Clone)]
 pub struct PhaseCountdowns {
     pub phase: Phase,
-    pub phase_ends_in: String, // FIXME: float doesn't work // not sure what the max is
+    #[serde_as(as = "DisplayFromStr")]
+    pub phase_ends_in: f64, // not sure what the max is
 }
