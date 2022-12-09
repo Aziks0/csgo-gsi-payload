5f22a52cba0dcf941650212418de6f330521b00e
Benjamin DIDIER <contact@aziks.aleeas.com>
2022-12-09 20:01:18 +0100

    feat: add round component

---
+++ src/round.rs
@@ -0,0 +1,20 @@
+use serde::Deserialize;
+
+use super::team::Team;
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+pub enum BombState {
+    Planted,
+    Exploded,
+    Defused,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+#[serde(tag = "phase")]
+pub enum Round {
+    FreezeTime,
+    Over { win_team: Team },
+    Live { bomb: Option<BombState> },
+}
